b302159fd339c960f2916daef422c53f9adfe88c
Alex <Alex@alex-mbp.home>
2014-03-30 17:29:39 +0200

    File renaming

--- SpringPendulum.js
+++ SpringPendulum.js
@@ -1,13 +1,21 @@
 "use strict";
 var SpringMassSystem;
+
 Initialize();
 
-var m = 10;
-var w0 = Math.sqrt(10/m);
-var d = 0.1;
-var sd = new SpringDynamics(m, w0, d);
-var x0 = 0.5;
-var v0 = -1;
+var k = 1; // spring constant
+var m = 1; // mass
+var y0 = 1.2; // initial condition - position
+var v0 = -1; // initial condition - velocity
+var w0 = Math.sqrt(k/m); // eigen-frequency
+//var w0 = 2;
+var d = 1; // dimensionless friction constant | d > 1 means strong friction (no oscillations) and d < 1 means low friction.
+var sd = new SpringDynamics(w0, d);
+var u0 = 2; // amp of suspension point
+var we = 10; // external frequency
+//var selectedDynamics = sd.getInitCondFunc(y0,v0);
+var selectedDynamics = sd.getExtForceFunc(u0,we);
+var mySpring = new Spring(selectedDynamics);
 
 function render(t) {
     
@@ -32,19 +40,26 @@ function render(t) {
     //var omega = Math.sqrt(k/m);
     //if (m == 0) 
     //   omega = 0;
+    var ut = -(u0*Math.sin(we*t/1000))
+    //SpringMassSystem = sd.getInitCondFunc(x0,v0)(t/500);//Math.abs(Math.cos(omega*t/1000));///
     
-    SpringMassSystem = sd.getInitCondFunc(x0,v0)(t/500);//Math.abs(Math.cos(omega*t/1000));
+    ceilingMesh.position.set(0,SpringPendulumConsts.offsetCeiling+ut,0);
+    
+    drawSpring(ut, t);
     
     mirrorSphere.visible = false;
     mirrorSphereCamera.updateCubeMap( renderer, scene );
     mirrorSphere.visible = true;
     
-    mirrorSphere.position.set(0,yFunc*5+offsetYdirToPlane+offsetYdirToCoil+radiusSegments+ceilingZ,0);
-
-    drawCoil();
+    mirrorSphere.position.set(0,SpringPendulumConsts.offsetCeiling+(SpringConsts.springLength*5*(1+selectedDynamics(t/1000)))+12,0);
+    
+    document.getElementById("debug").innerHTML = ""+(SpringConsts.springLength*5*(1+selectedDynamics(t/1000)));
+    
     renderer.render( scene, camera );
 }
+
 render();
+
 function CheckLanguage() {
     
     if(document.getElementById('RadioEngButton').checked) {
