43c3a6a4472efd5345b4952bb78a7aca29121888
Alex <Alex@alex-mbp.home>
2014-03-30 17:30:00 +0200

    Junemann's changes to the mathematical model

--- SpringDynamics.js
+++ SpringDynamics.js
@@ -5,60 +5,280 @@
  *
  * @constructor
  * @this {SpringDynamics}
- * @param {number} m mass in kg of pendulum mass.
  * @param {number} w0 eigenfrequency of the spring, must be > 0.
  * @param {number} d dimensionless friction constant, must be >= 0.
  */
-function SpringDynamics(m, w0, d) {
+
+
+
+function SpringDynamics(w0, d) {
     if(!(this instanceof SpringDynamics)) {
         return new SpringDynamics(w0, d);
     }
-    this.m = m;
+
+    // assert(w0>0);
+    // assert(d>=0);
     this.w0 = w0;
     this.d = d;
     this.TOL = 0.00001;
 }
 
 /**
- * creates function x(t) calculating the pendulum position as function of t for given
- * initial conditions x(0) = x0 and x'(0) = v0 without external force.
+ * creates function y(t) calculating the pendulum position as function of t for given
+ * initial conditions y(0) = y0 and y'(0) = v0 without external force.
  * @this {SpringDynamics}
- * @param {number} x0 initial position x(0)
- * @param {number} v0 initial speed x'(0)
- * @returns {function} function x(t) of a single parameter t giving the pendulum position at time t
+ * @param {number} y0 initial position y(0)
+ * @param {number} v0 initial speed y'(0)
+ * @returns {function} function y(t) of a single parameter t giving the pendulum position at time t
  */
-SpringDynamics.prototype.getInitCondFunc = function(x0, v0) {
+SpringDynamics.prototype.getInitCondFunc = function(y0, v0) {
     var ret;
     var that = this;
     var tmp, tmp2, tmp3, tmp4, tmp5;
+    var w0 = this.w0;
+    var d = this.d;
 
-    if(this.d < 1 - this.TOL) {
+    if(d < 1 - this.TOL) {
         // weak friction
-        tmp = Math.sqrt(1-this.d*this.d);
-        tmp2 = this.w0 * this.d;
-        tmp3 = this.w0 * tmp;
-        tmp4 = (v0 + this.d*this.w0*x0)/(this.w0*tmp);
+        tmp = Math.sqrt(1-d*d);
+        tmp2 = w0 * d;
+        tmp3 = w0 * tmp;
+        tmp4 = (v0 + d*w0*y0)/(w0*tmp);
         ret = function(t) {
-            return x0*Math.exp(-tmp2*t)*Math.cos(tmp3*t) +
+            return y0*Math.exp(-tmp2*t)*Math.cos(tmp3*t) +
                 tmp4*Math.exp(-tmp2*t)*Math.sin(tmp3*t);
         };
-    } else if(this.d > 1 + this.TOL) {
+    } else if(d > 1 + this.TOL) {
         // strong friction
-        tmp = Math.sqrt(this.d*this.d-1);
-        tmp2 = this.w0 * (this.d- tmp);
-        tmp3 = this.w0 * (this.d+ tmp);
-        tmp4 = (v0 +this.d*this.w0*x0+this.w0*x0*tmp) / (2*this.w0*tmp);
-        tmp5 = (v0 +this.d*this.w0*x0-this.w0*x0*tmp) / (2*this.w0*tmp);
+        tmp = Math.sqrt(d*d-1);
+        tmp2 = w0 * (d- tmp);
+        tmp3 = w0 * (d+ tmp);
+        tmp4 = (v0 +d*w0*y0+w0*y0*tmp) / (2*w0*tmp);
+        tmp5 = (v0 +d*w0*y0-w0*y0*tmp) / (2*w0*tmp);
         ret = function(t) {
             return tmp4 *Math.exp(-t*tmp2) - tmp5 * Math.exp(-t*tmp3);
         };
     } else {
         // limit case
-        tmp = v0 + x0*this.w0;
+        tmp = v0 + y0*w0;
         ret = function(t) {
-            return (x0 + t * tmp)* Math.exp(-that.w0 * t);
+            return (y0 + t * tmp)* Math.exp(-that.w0 * t);
         };
     }
+    return ret;
+};
+
+
+/**
+ * creates function h(t) calculating the impulse response of the pendulum
+ * @this {SpringDynamics}
+  * @returns {function} function h(t) of a single parameter t giving the pendulum position at time t
+ */
+SpringDynamics.prototype.getImpResp = function() {
+    var ret;
+    var that = this;
+    var tmp, tmp2, tmp3, tmp4;
+    var w0 = this.w0;
+    var d = this.d;
+
+    if(d < 1 - this.TOL) {
+        // weak friction
+        tmp = w0 * Math.sqrt(1-d*d);
+        tmp2 = w0 * d;
+        tmp3 = w0 / Math.sqrt(1-d*d);
+        ret = function(t) {
+            if(t<0) return 0;
+            else return tmp3 * Math.exp(-tmp2*t)*Math.sin(tmp*t);
+        };
+     } else if(d > 1 + this.TOL) {
+         // strong friction
+         tmp = Math.sqrt(d*d-1);
+         tmp2 = w0/(2 * tmp);
+         tmp3 = w0*(tmp-d);
+         tmp4 = w0*(-tmp-d);
+         ret = function(t) {
+             if(t<0) return 0;
+             else return tmp2*(Math.exp(tmp3*t) - Math.exp(tmp4*t));
+        };
+     } else {
+         // limit case
+         ret = function(t) {
+             if(t<0) return 0;
+             else return t*that.w0*that.w0*Math.exp(-that.w0*t);
+         };
+     }
+    return ret;
+};
+
+/**
+ * creates function h(t) calculating the impulse response of the pendulum
+ * @this {SpringDynamics}
+  * @returns {function} function h(t) of a single parameter t giving the pendulum position at time t
+ */
+SpringDynamics.prototype.getStepResp = function() {
+    var ret;
+    var tmp, tmp2, tmp3, tmp4;
+    var w0 = this.w0;
+    var d = this.d;
+
+    if(d < 1 - this.TOL) {
+        // weak friction
+        tmp = Math.sqrt(1-d*d);
+        tmp2 = w0 * tmp;
+        tmp3 = d/tmp;
+        ret = function(t) {
+            if(t<0) return 0;
+            else return 1 - Math.exp(-d*w0*t)*(Math.cos(tmp2*t) + tmp3*Math.sin(tmp2*t));
+        };
+     } else if(d > 1 + this.TOL) {
+         // strong friction
+         tmp = Math.sqrt(d*d-1);
+         tmp2 = 1+d / tmp;
+         tmp3 = 1-d / tmp;
+         tmp4 = w0*tmp;
+         ret = function(t) {
+             if(t<0) return 0;
+             else return 1- 0.5*Math.exp(-d*w0*t)*(tmp2* Math.exp(tmp4*t) + tmp3* Math.exp(-tmp4*t) );
+        };
+     } else {
+         // limit case
+         ret = function(t) {
+             if(t<0) return 0;
+             else return 1-(t*w0+1)*Math.exp(-w0*t);
+         };
+     }
+    return ret;
+};
+
+
+/**
+ * creates function y(t) calculating the pendulum position as function of t for vanishing
+ * initial conditions but with external force exerted by motion u(t) = u0*sin(we*t) of the
+ * upper suspension point of the pendulum.
+ * @this {SpringDynamics}
+ * @param {number} u0 amplitude of suspension point
+ * @param {number} we external frequency
+ * @returns {function} function y(t) of a single parameter t giving the pendulum position at time t
+ */
+SpringDynamics.prototype.getExtForceFunc = function(u0, we) {
+    var ret;
+    var that = this;
+    var tmp, tmp2, tmp3, tmp4;
+    var w0 = this.w0;
+    var d = this.d;
+
+    if(d < 1 - this.TOL) {
+        tmp = Math.sqrt(1-d*d);  // tmp=D
+        tmp2 = w0*d;
+        // tmp3=A
+        tmp3 = u0 * w0*w0 / ((4*d*d-2)*w0*w0*we*we
+                                       + w0*w0*w0*w0 +we*we*we*we);
+        tmp4 = ((2*d*d-1)*w0*w0+we*we)/(2*tmp2*w0*tmp);
+
+        ret = function(t) {
+            return 2*tmp2*we*tmp3*Math.exp(-tmp2*t)
+                * (Math.cos(w0*tmp*t) + tmp4*Math.sin(w0*tmp*t))
+                - tmp3 * (we*we-w0*w0)*Math.sin(we*t)
+                - 2 * tmp3 *tmp2*we*Math.cos(we*t);
+        };
+    } else if(d > 1 + this.TOL) {
+
+        tmp = Math.sqrt(d*d-1);   // = D
+        tmp2 = u0*w0*w0/(4 * d*d*w0*w0*we*we+w0*w0*w0*w0-2*w0*w0*we*we+we*we*we*we);  // A
+        tmp3 = ((2*d*d-1)*w0*w0+we*we)/(2*d*w0*w0*tmp);
+
+        ret = function(t) {
+            return d*tmp2*w0*we*Math.exp(-t*d*w0)*(Math.exp(t*w0*tmp)*(1+tmp3)
+                                                   +Math.exp(-t*w0*tmp)*(1-tmp3))
+            +tmp2*((w0*w0-we*we)*Math.sin(we*t)-2*d*w0*we*Math.cos(we*t));
+        };
+    } else {
+        // limit case
+        tmp = w0*w0 + we*we;
+        tmp2 = u0*w0*w0/tmp;   // = B
+        tmp3 = tmp2 / tmp;  // = A
+
+        ret = function(t) {
+            return (2*tmp3*w0+t*tmp2)*we*Math.exp(-t*w0)
+            + tmp3 * (w0*w0 - we*we) * Math.sin(t*we)
+            - 2 * tmp3 * w0*we* Math.cos(t*we);
+         };
+     }
+
+    return ret;
+};
+
+
+/**
+ * creates function y(t) calculating the pendulum position as function of t with
+ * initial conditions and with external force exerted by motion u(t) = u0*sin(we*t) of the
+ * upper suspension point of the pendulum. It is just the sum of getInitCondFunc and getExtForceFunc.
+ * @this {SpringDynamics}
+ * @param {number} u0 amplitude of suspension point
+ * @param {number} we external frequency
+ * @param {number} y0 initial position y(0)
+ * @param {number} v0 initial speed y'(0)
+ * @returns {function} function y(t) of a single parameter t giving the pendulum position at time t
+ */
+SpringDynamics.prototype.getPositionFunc = function(u0, we, y0, v0) {
+    var f1 = this.getInitCondFunc(y0, v0);
+    var f2 = this.getExtForceFunc(u0, we);
+    var ret = function(t) {
+        return f1(t) + f2(t);
+    };
+    return ret;
+};
+
+
+/**
+ * creates function func(w) calculating the magnitude response of the system as a function of the
+ * frequency w. A flag of the returned function controls whether the magnitude response |H| or
+ * the logarithmic quantity
+ * -20*log10(|H|) is calculated.
+ * @this {SpringDynamics}
+ * @returns {function} function func w of a single parameter w giving the magnitude response
+ */
+SpringDynamics.prototype.getMagRespFunc = function() {
+
+    var ret;
+    var w02 = this.w0*this.w0;
+    var d2 = this.d*this.d;
+
+    // the dB flag indicates whether or not to return a logarithmic magnitude response
+    ret = function(w, dB) {
+        if(dB===undefined) dB = true;
+        var w2 = w*w;
+        var absH = w02 / Math.sqrt((4*d2-2)*w02*w2 + w2*w2 + w02*w02);
+        if(dB) {
+            return 20*Math.LOG10E*Math.log(absH);
+        } else {
+            return absH;
+        }
+        return 0;
+    };
+    return ret;
+};
+
+/**
+ * creates function func(w) calculating the phase response in degrees of the system as a function of the
+ * frequency w.
+ * @this {SpringDynamics}
+ * @returns {function} function func w of a single parameter w giving the phase response
+ */
+SpringDynamics.prototype.getPhaseRespFunc = function(dB) {
+    var ret;
+    var w0 = this.w0;
+    var d = this.d;
+    var w02 = this.w0*this.w0;
+    var d2 = this.d*this.d;
+
+    ret = function(w) {
+        var w2 = w*w;
+        var den = (w02-w2)*(w02-w2) + 4*d2*w2*w02;
+        var real =  w02*(w02-w2)/den;
+        var imag = -2*d*w*w02*w0 / den;
+        return Math.atan2(imag, real) * 180 / Math.PI;
+    };
     return ret;
 };
 
@@ -78,14 +298,14 @@ SpringDynamics.prototype.getPoles = function() {
     poles[0] = [0,0];
     poles[1] = [0,0];
 
-    if(this.d < 1 - this.TOL) {
+    if(d < 1 - this.TOL) {
     // weak friction: poles with imaginary parts
         poles[0][0] = -w0*d;
         poles[0][1] =  w0*Math.sqrt(1-d*d);
         poles[1][0] = -w0*d;
         poles[1][1] = -w0*Math.sqrt(1-d*d);
 
-    } else if(this.d > 1 + this.TOL) {
+    } else if(d > 1 + this.TOL) {
     // strong friction: no imaginary parts
         poles[0][0] = -w0*(d - Math.sqrt(d*d-1));
         poles[1][0] = -w0*(d + Math.sqrt(d*d-1));
--- SpringDynamics_jsxgraph.js
+++ SpringDynamics_jsxgraph.js
@@ -1,7 +1,6 @@
-var m = 1;
-var w0 = 2;
-var d = 1;
-var sd = new SpringDynamics(m, w0, d);
+var w0 = 5;
+var d = 0.5;
+var sd = new SpringDynamics(w0, d);
 
 var brd1 = JXG.JSXGraph.initBoard('jxgbox1',
                                   {boundingbox:[-0.1, 1.2, 5, -1.2],
@@ -12,14 +11,14 @@ var brd1 = JXG.JSXGraph.initBoard('jxgbox1',
 
 
 var x0 = 0.5;
-var v0 = -1;
+var v0 = 1;
 var func = sd.getInitCondFunc(x0, v0);
 brd1.create('functiongraph',
             [func],
             {strokeColor:'blue', strokeWidth:2});
 
 d = 2;
-sd = new SpringDynamics(m, w0, d);
+sd = new SpringDynamics(w0, d);
 var brd2 = JXG.JSXGraph.initBoard('jxgbox2',
                                   {boundingbox:[-0.1, 1.2, 5, -1.2],
                                   keepaspectratio:false,
@@ -32,7 +31,7 @@ brd2.create('functiongraph',
             {strokeColor:'blue', strokeWidth:2});
 
 d = 0.2;
-sd = new SpringDynamics(m, w0, d);
+sd = new SpringDynamics(w0, d);
 var brd3 = JXG.JSXGraph.initBoard('jxgbox3',
                                   {boundingbox:[-0.1, 1.2, 5, -1.2],
                                   keepaspectratio:false,
@@ -43,3 +42,126 @@ func = sd.getInitCondFunc(x0, v0);
 brd3.create('functiongraph',
             [func],
             {strokeColor:'blue', strokeWidth:2});
+
+
+
+d=2;
+sd = new SpringDynamics(w0, d);
+var brd4 = JXG.JSXGraph.initBoard('jxgbox4',
+                                  {boundingbox:[-0.1, 1.2, 5, -1.2],
+                                  keepaspectratio:false,
+                                  axis:true,
+                                  showCopyright: false,
+                                  showNavigation: false});
+func = sd.getImpResp();
+brd4.create('functiongraph',
+            [func],
+            {strokeColor:'blue', strokeWidth:2});
+
+d=1;
+sd = new SpringDynamics(w0, d);
+var brd5 = JXG.JSXGraph.initBoard('jxgbox5',
+                                  {boundingbox:[-0.1, 1.2, 5, -1.2],
+                                  keepaspectratio:false,
+                                  axis:true,
+                                  showCopyright: false,
+                                  showNavigation: false});
+func = sd.getImpResp();
+brd5.create('functiongraph',
+            [func],
+            {strokeColor:'blue', strokeWidth:2});
+d=0.2;
+sd = new SpringDynamics(w0, d);
+var brd6 = JXG.JSXGraph.initBoard('jxgbox6',
+                                  {boundingbox:[-0.1, 1.2, 5, -1.2],
+                                  keepaspectratio:false,
+                                  axis:true,
+                                  showCopyright: false,
+                                  showNavigation: false});
+func = sd.getImpResp();
+brd6.create('functiongraph',
+            [func],
+            {strokeColor:'blue', strokeWidth:2});
+
+var brd7 = JXG.JSXGraph.initBoard('jxgbox7',
+                                  {boundingbox:[-0.1, 3.2, 5, -3.2],
+                                  keepaspectratio:false,
+                                  axis:true,
+                                  showCopyright: false,
+                                  showNavigation: false});
+var u0 = 1.4;
+var we = 1.6;
+func = sd.getExtForceFunc(u0, we);
+brd7.create('functiongraph',
+            [func],
+            {strokeColor:'blue', strokeWidth:2});
+
+sd = new SpringDynamics(2,0.2);
+func = sd.getStepResp();
+var brd8 = JXG.JSXGraph.initBoard('jxgbox8',
+                                  {boundingbox:[-0.1, 3.2, 5, -3.2],
+                                  keepaspectratio:false,
+                                  axis:true,
+                                  showCopyright: false,
+                                  showNavigation: false});
+
+brd8.create('functiongraph',
+            [func],
+            {strokeColor:'blue', strokeWidth:2});
+
+
+
+u0 = 1.4;
+we = 1.6;
+var y0 = 0.5;
+v0 = 1.3;
+sd = new SpringDynamics(2, 1.8);
+func = sd.getPositionFunc(u0, we, y0,v0);
+var brd9= JXG.JSXGraph.initBoard('jxgbox9',
+                                  {boundingbox:[-0.1, 3.2, 5, -3.2],
+                                  keepaspectratio:false,
+                                  axis:true,
+                                  showCopyright: false,
+                                  showNavigation: false});
+
+brd9.create('functiongraph',
+            [func],
+            {strokeColor:'blue', strokeWidth:2});
+
+
+sd = new SpringDynamics(2, 0.2);
+func = sd.getMagRespFunc();
+var brd10 = JXG.JSXGraph.initBoard('jxgbox10',
+                                  {boundingbox:[-0.1, 5, 5, -3],
+                                  keepaspectratio:false,
+                                  axis:true,
+                                  showCopyright: false,
+                                  showNavigation: false});
+
+brd10.create('functiongraph',
+             [function(w) {return func(w,false);}],
+             {strokeColor:'blue', strokeWidth:2});
+
+
+var brd11 = JXG.JSXGraph.initBoard('jxgbox11',
+                                  {boundingbox:[-0.1, 10, 5, -10],
+                                  keepaspectratio:false,
+                                  axis:true,
+                                  showCopyright: false,
+                                  showNavigation: false});
+
+brd11.create('functiongraph',
+             [func],
+             {strokeColor:'blue', strokeWidth:2});
+
+func = sd.getPhaseRespFunc();
+var brd12 = JXG.JSXGraph.initBoard('jxgbox12',
+                                  {boundingbox:[-0.1, 10, 5, -200],
+                                  keepaspectratio:false,
+                                  axis:true,
+                                  showCopyright: false,
+                                  showNavigation: false});
+
+brd12.create('functiongraph',
+             [func],
+             {strokeColor:'blue', strokeWidth:2});
